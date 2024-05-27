9c9448fb20b785728d8d3ceb107a8b4fc5049bf5
김은채 <ke808762@gmail.com>
2024-05-27 15:16:04 +0900

    first commit

---
+++ src/main/java/team9502/sinchulgwinong/SinChulGwiNongApplication.java
@@ -0,0 +1,13 @@
+package team9502.sinchulgwinong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SinChulGwiNongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SinChulGwiNongApplication.class, args);
+    }
+
+}
---
+++ src/test/java/team9502/sinchulgwinong/SinChulGwiNongApplicationTests.java
@@ -0,0 +1,13 @@
+package team9502.sinchulgwinong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SinChulGwiNongApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
