aec934ce97c0c4b2881b17252c5dd48614ddda64
김은채 <ke808762@gmail.com>
2024-06-09 23:09:33 +0900

    hotfix: 헤더 수정

--- src/main/java/team9502/sinchulgwinong/global/config/CorsConfig.java
+++ src/main/java/team9502/sinchulgwinong/global/config/CorsConfig.java
@@ -19,6 +19,7 @@ public class CorsConfig {
         config.addAllowedOrigin("http://localhost:3000");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
+        config.addExposedHeader("Authorization");
 
         source.registerCorsConfiguration("/**", config);
 
