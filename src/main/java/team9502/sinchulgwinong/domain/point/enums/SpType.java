12a66d61f56f35322a3ad0605561ff567152c218
김은채 <ke808762@gmail.com>
2024-06-11 01:46:13 +0900

    add: 유형에 대한 주석 추가

--- src/main/java/team9502/sinchulgwinong/domain/point/enums/SpType.java
+++ src/main/java/team9502/sinchulgwinong/domain/point/enums/SpType.java
@@ -2,5 +2,15 @@ package team9502.sinchulgwinong.domain.point.enums;
 
 public enum SpType {
 
+    /*
+        REVIEW: 리뷰 작성
+        SIGNUP: 회원가입
+        BOARD: 게시글 작성
+        EVENT: 이벤트 참여
+        CHAT: 채팅
+        FIRST_BOARD: 첫 게시글 작성
+        JOBS: 채용글 작성
+     */
+
     REVIEW, SIGNUP, BOARD, EVENT, CHAT, FIRST_BOARD, JOBS
 }
--- src/main/java/team9502/sinchulgwinong/domain/point/enums/UpType.java
+++ src/main/java/team9502/sinchulgwinong/domain/point/enums/UpType.java
@@ -2,5 +2,12 @@ package team9502.sinchulgwinong.domain.point.enums;
 
 public enum UpType {
 
+    /*
+        REVIEW: 리뷰
+        BANNER: 배너
+        PROFILE: 프로필
+        TOP: 최상위 노출
+     */
+
     REVIEW, BANNER, PROFILE, TOP
 }
