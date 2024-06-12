be3bf6c95000c882d641cc7f701c09dcb94892b0
de123456sdf <cdaeun95@gmail.com>
2024-06-13 01:03:12 +0900

    fix: @FutureOrPresent 추가

--- src/main/java/team9502/sinchulgwinong/domain/jobBoard/dto/request/JobBoardRequestDTO.java
+++ src/main/java/team9502/sinchulgwinong/domain/jobBoard/dto/request/JobBoardRequestDTO.java
@@ -1,5 +1,6 @@
 package team9502.sinchulgwinong.domain.jobBoard.dto.request;
 
+import jakarta.validation.constraints.FutureOrPresent;
 import lombok.Getter;
 import team9502.sinchulgwinong.domain.jobBoard.entity.JobStatus;
 import team9502.sinchulgwinong.domain.jobBoard.entity.SalaryType;
@@ -13,8 +14,10 @@ public class JobBoardRequestDTO {
 
     private String jobContent;
 
+    @FutureOrPresent
     private LocalDate jobStartDate;
 
+    @FutureOrPresent
     private LocalDate jobEndDate;
 
     private Integer salaryAmount;
