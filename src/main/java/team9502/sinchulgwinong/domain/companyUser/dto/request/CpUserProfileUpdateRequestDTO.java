c28123525f7ce03dd551ee4012ec8cc17a90647d
김은채 <ke808762@gmail.com>
2024-06-13 23:07:49 +0900

    feat: 유효성 검사 추가

--- src/main/java/team9502/sinchulgwinong/domain/companyUser/dto/request/CpUserProfileUpdateRequestDTO.java
+++ src/main/java/team9502/sinchulgwinong/domain/companyUser/dto/request/CpUserProfileUpdateRequestDTO.java
@@ -1,6 +1,7 @@
 package team9502.sinchulgwinong.domain.companyUser.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -23,5 +24,6 @@ public class CpUserProfileUpdateRequestDTO {
     private String cpEmail;
 
     @Schema(description = "기업 전화번호", example = "01012345678")
+    @Size(min = 11, max = 11, message = "전화번호는 11자리로 입력해주세요.")
     private String cpPhoneNumber;
 }
