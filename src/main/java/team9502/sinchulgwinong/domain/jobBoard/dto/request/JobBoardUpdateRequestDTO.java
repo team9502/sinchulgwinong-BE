66e37fda58717267afd5c0f1b9a14f0064cb95d5
de123456sdf <cdaeun95@gmail.com>
2024-06-19 23:12:15 +0900

    fix: update 전용 requestDTO 생성

---
+++ src/main/java/team9502/sinchulgwinong/domain/jobBoard/dto/request/JobBoardUpdateRequestDTO.java
@@ -0,0 +1,32 @@
+package team9502.sinchulgwinong.domain.jobBoard.dto.request;
+
+import jakarta.validation.constraints.FutureOrPresent;
+import lombok.Getter;
+import team9502.sinchulgwinong.domain.jobBoard.entity.JobStatus;
+import team9502.sinchulgwinong.domain.jobBoard.entity.SalaryType;
+
+import java.time.LocalDate;
+
+@Getter
+public class JobBoardUpdateRequestDTO {
+
+    private String jobTitle;
+
+    private String jobContent;
+
+    @FutureOrPresent
+    private LocalDate jobStartDate;
+
+    @FutureOrPresent
+    private LocalDate jobEndDate;
+
+    private Integer salaryAmount;
+
+    private String sex;
+
+    private String address;
+
+    private JobStatus jobStatus;
+
+    private SalaryType salaryType;
+}
