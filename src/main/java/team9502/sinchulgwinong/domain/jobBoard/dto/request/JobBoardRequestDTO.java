de932d69564ad209220a946497bb23be57181028
de123456sdf <cdaeun95@gmail.com>
2024-06-19 23:13:39 +0900

    fix: 유효성 검사 추가

--- src/main/java/team9502/sinchulgwinong/domain/jobBoard/dto/request/JobBoardRequestDTO.java
+++ src/main/java/team9502/sinchulgwinong/domain/jobBoard/dto/request/JobBoardRequestDTO.java
@@ -1,8 +1,8 @@
 package team9502.sinchulgwinong.domain.jobBoard.dto.request;
 
 import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
-import team9502.sinchulgwinong.domain.jobBoard.entity.JobStatus;
 import team9502.sinchulgwinong.domain.jobBoard.entity.SalaryType;
 
 import java.time.LocalDate;
@@ -10,23 +10,29 @@ import java.time.LocalDate;
 @Getter
 public class JobBoardRequestDTO {
 
+    @NotEmpty
     private String jobTitle;
 
+    @NotEmpty
     private String jobContent;
 
+    @NotEmpty
     @FutureOrPresent
     private LocalDate jobStartDate;
 
+    @NotEmpty
     @FutureOrPresent
     private LocalDate jobEndDate;
 
+    @NotEmpty
     private Integer salaryAmount;
 
+    @NotEmpty
     private String sex;
 
+    @NotEmpty
     private String address;
 
-    private JobStatus jobStatus;
-
+    @NotEmpty
     private SalaryType salaryType;
 }
