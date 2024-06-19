47ab20e5613e6f1eab662595c46c19cc3915a03b
de123456sdf <cdaeun95@gmail.com>
2024-06-19 15:57:22 +0900

    fix: 전체 조회 응답시 댓글 총 갯수도 포함해서 응답하도록 수정

--- src/main/java/team9502/sinchulgwinong/domain/comment/repository/CommentRepository.java
+++ src/main/java/team9502/sinchulgwinong/domain/comment/repository/CommentRepository.java
@@ -1,7 +1,7 @@
 package team9502.sinchulgwinong.domain.comment.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
 import team9502.sinchulgwinong.domain.comment.entity.Comment;
 
 import java.util.List;
@@ -9,4 +9,9 @@ import java.util.List;
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
     List<Comment> findByBoard_BoardId(Long boardId);
+
+    List<Comment> findByUser_UserId(Long userId);
+
+    @Query("SELECT COUNT(c) FROM Comment c WHERE c.board.boardId = :boardId")
+    Long countCommentsByBoardId(Long boardId);
 }
