7e8c023c6010f18954912cf02f5b0be8b2244544
de123456sdf <cdaeun95@gmail.com>
2024-06-21 14:03:09 +0900

    fix: 필요없는 어노테이션 제거

--- src/main/java/team9502/sinchulgwinong/domain/board/repository/BoardRepository.java
+++ src/main/java/team9502/sinchulgwinong/domain/board/repository/BoardRepository.java
@@ -3,10 +3,8 @@ package team9502.sinchulgwinong.domain.board.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import team9502.sinchulgwinong.domain.board.entity.Board;
 
-@Repository
 public interface BoardRepository extends JpaRepository<Board, Long> {
 
     Page<Board> findByUser_UserId(Long userId, Pageable pageable);
