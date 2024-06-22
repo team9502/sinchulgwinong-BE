c34923fcc2f77c24667e5c3a671be8fa0677cf02
김은채 <ke808762@gmail.com>
2024-06-23 00:15:02 +0900

    refactor: 애너테이션 제거

--- src/main/java/team9502/sinchulgwinong/domain/faq/dto/request/FaqUpdateRequestDTO.java
+++ src/main/java/team9502/sinchulgwinong/domain/faq/dto/request/FaqUpdateRequestDTO.java
@@ -3,11 +3,9 @@ package team9502.sinchulgwinong.domain.faq.dto.request;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Data
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
