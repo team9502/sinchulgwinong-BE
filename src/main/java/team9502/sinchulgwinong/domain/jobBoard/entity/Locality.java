149eeb73bdd4180308b1ac621186b1821d38e733
de123456sdf <cdaeun95@gmail.com>
2024-06-25 04:51:41 +0900

    fix: 지역 카테고리 entity, repository 생성

---
+++ src/main/java/team9502/sinchulgwinong/domain/jobBoard/entity/Locality.java
@@ -0,0 +1,22 @@
+package team9502.sinchulgwinong.domain.jobBoard.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Getter
+@Entity
+public class Locality {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long localityId;
+
+    @Column(nullable = false)
+    private String regionName;
+
+    @Column(nullable = false)
+    private String subRegionName;
+
+    @Column(nullable = false)
+    private String localityName;
+}
