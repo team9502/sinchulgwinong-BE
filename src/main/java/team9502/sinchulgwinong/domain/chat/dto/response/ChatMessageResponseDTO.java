682ba9e1176c7c1db828d8ccc9d65f71922e183a
de123456sdf <cdaeun95@gmail.com>
2024-06-29 21:58:04 +0900

    fix: 생성 시간 response에 추가

--- src/main/java/team9502/sinchulgwinong/domain/chat/dto/response/ChatMessageResponseDTO.java
+++ src/main/java/team9502/sinchulgwinong/domain/chat/dto/response/ChatMessageResponseDTO.java
@@ -1,7 +1,11 @@
 package team9502.sinchulgwinong.domain.chat.dto.response;
 
+import lombok.Getter;
 import team9502.sinchulgwinong.domain.chat.entity.ChatMessage;
 
+import java.time.LocalDateTime;
+
+@Getter
 public class ChatMessageResponseDTO {
 
     private Long chatMessageId;
@@ -14,10 +18,13 @@ public class ChatMessageResponseDTO {
 
     private String message;
 
+    private LocalDateTime createAt;
+
     public ChatMessageResponseDTO(ChatMessage chatMessage) {
         this.chatMessageId = chatMessage.getChatMessageId();
         this.chatRoomId = chatMessage.getChatRoom().getChatRoomId();
         this.message = chatMessage.getMessage();
+        this.createAt = chatMessage.getCreatedAt();
 
         if(chatMessage.getCompanyUser() != null){
             this.cpUserId = chatMessage.getCompanyUser().getCpUserId();
