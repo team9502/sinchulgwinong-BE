1cd350bde327031c99f0c7be83fae7a6f264dd9c
김은채 <ke808762@gmail.com>
2025-02-17 18:19:11 +0900

    test: 구인자(기업 회원) 테스트 코드 추가

--- src/main/java/team9502/sinchulgwinong/global/exception/ApiException.java
+++ src/main/java/team9502/sinchulgwinong/global/exception/ApiException.java
@@ -9,9 +9,12 @@ public class ApiException extends RuntimeException {
     private final HttpStatus httpStatus;
     private final String message;
 
+    private final ErrorCode errorCode;
+
     public ApiException(ErrorCode errorCode) {
 
         super(errorCode.getMessage());
+        this.errorCode = errorCode;
         this.httpStatus = errorCode.getHttpStatus();
         this.message = errorCode.getMessage();
     }
